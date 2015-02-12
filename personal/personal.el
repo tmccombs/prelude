c5d1b4b5c401aeced9e8f5d4ec859b8d9ec04159
Thayne McCombs <astrothayne@gmail.com>
2015-02-11 19:54:08 -0700

    Add personal customizations to git.

---
+++ personal/personal.el
@@ -0,0 +1,42 @@
+;;; personal -- personal customizations
+;;;
+;;; Commentary:
+;;; Personal emacs configurations
+;;;
+;;; Code:
+
+;;; User customization
+(setq user-mail-address "astrothayne@gmail.com")
+
+;;; Additional package archives
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+;;; use utf-8
+(prefer-coding-system 'utf-8)
+(setq coding-system-for-read 'utf-8)
+(setq coding-system-for-write 'utf-8)
+
+;;; Set some variables
+(global-linum-mode 1)
+(show-paren-mode 1)
+(menu-bar-mode 1)
+
+;;; YASnippet
+(setq yas-snippet-dirs '("~/.emacs.d/personal/snippets" yas-installed-snippets-dir))
+(add-hook 'yas-minor-mode-hook
+          (lambda ()
+            (yas-activate-extra-mode 'fundamental-mode)))
+
+;; Fix doc-view
+(add-hook 'doc-view-mode-hook
+          (lambda ()
+            (linum-mode -1)))
+
+;;; Slime setup
+(setq slime-contribs '(slime-fancy ))
+
+;; Alter default modes
+(push '("\\.m$" . octave-mode) auto-mode-alist)
+
+(provide 'personal)
+;;; personal.el ends here
