feb9d172884b7ad1baf8c2312f9cc61a52575e7d
Thayne McCombs <astrothayne@gmail.com>
2015-02-13 21:45:59 -0700

    Add maxima and multiple cursors to personal

---
+++ personal/maxima.el
@@ -0,0 +1,15 @@
+;;; maxima-autoload -- Add auto load lines for maxima
+;;;
+;;; Commentary:
+;;; Set up auto load lines for maxima modes
+;;;
+;;; Code:
+
+(autoload 'maxima-mode "maxima" "Maxima mode" t)
+(autoload 'imaxima "imaxima" "Frontend for maxima with Image support" t)
+(autoload 'maxima "maxima" "Maxima interaction" t)
+(autoload 'imath-mode "imath" "Imath mode for math formula input" t)
+(setq imaxima-use-maxima-mode-flag t)
+(add-to-list 'auto-mode-alist '("\\.ma[cx]" . maxima-mode))
+
+;;; maxima.el ends here
